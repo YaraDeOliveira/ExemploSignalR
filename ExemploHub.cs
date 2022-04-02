5d3c3e0c21778dbfc427251fe45a362e51138d77
Yara <yara.regina@hotmail.com>
2022-04-02 13:50:26 -0300

    Preparando o Hub

---
+++ ExemploHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploSignalR {
+    public class ExemploHub : Hub {
+        public void EnviarMensagemParaHub(string msg) {
+            Clients.All.enviarMensagemParaClient(msg);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ExemploSignalR.Startup))]
+
+namespace ExemploSignalR {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.MapSignalR();
+        }
+    }
+}
